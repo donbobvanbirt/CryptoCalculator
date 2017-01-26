40824a3c69cb49c22cbf8cf898b64fb0abcece18
Donovan <donovan6925@gmail.com>
2017-01-26 17:12:03 -0500

    cleaning up

--- src/actions/MarketActions.js
+++ src/actions/MarketActions.js
@@ -1,22 +1,20 @@
 import axios from 'axios';
 
 export function fetchPrice(exchange) {
+  let payload;
   if (exchange === 'Bitfinex') {
-    return {
-      type: 'FETCH_PRICE',
-      payload: axios.get('https://api.bitfinex.com/v1/pubticker/BTCUSD').then(res => res.data),
-    };
+    payload = axios.get('https://api.bitfinex.com/v1/pubticker/BTCUSD').then(res => res.data);
   } else if (exchange === 'Bitstamp') {
     payload = axios.get('https://www.bitstamp.net/api/ticker/')
       .then((res) => {
         const priceObj = res.data;
         priceObj.last_price = res.data.last;
-        console.log('priceObj:', priceObj);
+        // console.log('priceObj:', priceObj);
         return priceObj;
       });
-    return {
-      type: 'FETCH_PRICE',
-      payload,
-    };
   }
+  return {
+    type: 'FETCH_PRICE',
+    payload,
+  };
 }
