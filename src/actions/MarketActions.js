5863355f2ea02a3e09cd4bf4124c17fdc1ac6f42
Donovan <donovan6925@gmail.com>
2017-10-03 19:40:08 -0400

    added new action

--- src/actions/MarketActions.js
+++ src/actions/MarketActions.js
@@ -1,8 +1,19 @@
 const coinTicker = require('coin-ticker');
 
 export function fetchPrice(exchange, pair) {
+  const payload = coinTicker(exchange.toLowerCase().split('-').join(''), pair.split('/').join('_'));
+  // console.log('payload:', payload);
   return {
     type: 'FETCH_PRICE',
-    payload: coinTicker(exchange.toLowerCase().split('-').join(''), pair.toLowerCase().split('/').join('')),
+    payload,
+  };
+}
+
+export function fetchExchanges() {
+  const payload = coinTicker();
+  console.log('payload:', payload);
+  return {
+    type: 'FETCH_EXCHANGES',
+    payload,
   };
 }
